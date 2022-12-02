6835aef7ae5f93f5f150bb6e9f509b3b1393726f
sshah83 <sshah@lightboxre.com>
2022-12-02 08:08:09 -0800

    Added map example and component library
    Added map example and component library

---
+++ my-app/src/App.tsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import { MapExample } from "./MapExample"
+
+function App() {
+  return (
+    <div style={{ justifyContent: 'center'}}>
+    <p>Map POC Example</p>
+    <MapExample />
+    </div>
+  )
+} 
+
+export default App;
---
+++ my-app/src/MapExample.tsx
@@ -0,0 +1,27 @@
+import { 
+  Map, 
+  MapInstanceScope, 
+  ZoomControlPosition
+} from "@sshah83/components";
+import { createMapScope } from "./creators/createMapScope";
+
+/**
+ * This is the main rendered MapExample map component that's used in the MapExample application.
+ * This map component has only a single scope that's loaded in {@link MapExample} and used to initialize this component.
+ */
+
+export const MapExample = () => {
+
+  const scope = createMapScope();
+  return (
+    <MapInstanceScope scope={scope}>
+      <Map
+        scope={scope}
+        height="400px"
+        width="100%"
+      >
+      <ZoomControlPosition position={"bottomright"} />
+    </Map>
+      </MapInstanceScope>
+  );
+};
